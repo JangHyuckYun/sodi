9a84506723203c93f05983e22390dd3d695fd236
JangHyuckYun <skg09203@naver.com>
2022-09-24 10:04:56 +0900

    first commit

---
+++ src/test/java/com/sodi/sodi/SodiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sodi.sodi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SodiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
