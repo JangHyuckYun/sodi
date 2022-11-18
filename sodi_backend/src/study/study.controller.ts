a8197f0fe303971d2a2935fee0f1b386013ca537
JangHyuck Yun <skg09203@naver.com>
2022-11-18 09:36:55 +0900

    Delete sodi_backend directory

--- sodi_backend/src/study/study.controller.ts
+++
@@ -1,4 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('study')
-export class StudyController {}
--- sodi_backend/src/study/study.module.ts
+++
@@ -1,7 +0,0 @@
-import { Module } from '@nestjs/common';
-import { StudyController } from './study.controller';
-
-@Module({
-  controllers: [StudyController]
-})
-export class StudyModule {}
